836c486ebac3411cad003cd7c33c50453cd0fc09
afoolsbag <afoolsbag@gmail.com>
2020-09-01 10:11:36 +0800

    feat(rrexenut3): 变动若干条产品昵称

--- rrexenut3/lib/rrexenut3/ifrs/nicknames.rb
+++ rrexenut3/lib/rrexenut3/ifrs/nicknames.rb
@@ -2,18 +2,19 @@
 # frozen_string_literal: true
 
 # zhengrr
-# 2020-08-28 – 2020-08-28
+# 2020-08-28 – 2020-09-01
 # Unlicense
 
 module RrExeNut3
   module Ifrs
     # @return [Hash{String=>String}]
     NICKNAMES = {
+      '鸡蛋' => 'CN.CDC.FCD.978',
       '热干面' => 'CN.CDC.FCD.1213',
-      '怡宝' => 'CN.NHC.LPF.6901285991486',
+      '怡宝' => 'CN.NHC.LPF.6901285991219',
       '纤维雪碧' => 'CN.NHC.LPF.6928804016567',
       '纤维可乐' => 'CN.NHC.LPF.6928804016970',
-      '牛肉干' => 'CN.NHC.LPF.6970479070061',
+      '牛肉干' => 'CN.NHC.LPF.6970479070061'
     }.freeze
   end
 end
