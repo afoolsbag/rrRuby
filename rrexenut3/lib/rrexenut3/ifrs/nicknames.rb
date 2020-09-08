b3585e0a322564f9a76f0aeb58f8e05364e747de
afoolsbag <afoolsbag@gmail.com>
2020-09-08 12:19:28 +0800

    feat: 更新数据库

--- rrexenut3/lib/rrexenut3/ifrs/nicknames.rb
+++ rrexenut3/lib/rrexenut3/ifrs/nicknames.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 # zhengrr
-# 2020-08-28 – 2020-09-01
+# 2020-08-28 – 2020-09-08
 # Unlicense
 
 module RrExeNut3
@@ -13,6 +13,7 @@ module RrExeNut3
       '鸡蛋' => 'CN.CDC.FCD.978',
       '热干面' => 'CN.CDC.FCD.1213',
       '怡宝' => 'CN.NHC.LPF.6901285991219',
+      '百岁山' => 'CN.NHC.LPF.6922255451427',
       '纤维雪碧' => 'CN.NHC.LPF.6928804016567',
       '纤维可乐' => 'CN.NHC.LPF.6928804016970',
       '牛肉干' => 'CN.NHC.LPF.6970479070061'
